db2214a131b9e07d51429b855cae0af13ab89aa1
CrazyJugg <CrazyJugg>
2018-04-23 17:01:03 +0800

    new

--- NewC_Structure/LinkList.c
+++ NewC_Structure/LinkList.c
@@ -3,48 +3,146 @@
 //初始化链表
 LinkList* Init_LinkList()
 {
+	LinkList* list=(LinkList*)malloc(sizeof(LinkList));
+	list->size=0;
 
-	return NULL;
+	//头结点 是不保存数据信息
+	list->head=(LinkNode*)malloc(sizeof(LinkNode));
+	list->head->data=NULL;
+	list->head->next=NULL;
+	return list;
 }
 //指定位置插入
 void Insert_LinkList(LinkList* list,int pos,void *data)
 {
+	int i;
+	LinkNode* newnode=(LinkNode*)malloc(sizeof(LinkNode));
+	LinkNode* pCurrent=list->head;
+	if(NULL==list)
+	{
+		return;
+	}
+	
+	if(NULL==data)
+	{
+		return;
+	}
 
+	//友好处理，pos越界
+	if(pos<0 || pos>list->size)
+	{
+		pos=list->size;
+	}
+	
+	//创建新的结点
+	newnode->data=data;
+	newnode->next=NULL;
+	//找节点
+	for(i=0;i<pos;i++)
+	{
+		pCurrent=pCurrent->next;
+	}
 
+	//新结点入链表
+	newnode->next=pCurrent->next;
+	pCurrent->next=newnode;
+
+	list->size++;
 }
 //删除指定位置的值
 void RemoveByPos_LinkList(LinkList* list,int pos)
 {
+	int i;
+	LinkNode* pCurrent=list->head;
+	LinkNode* pDel;
+	if(NULL==list)
+	{
+		return;
+	}
+	//查找删除结点前一个结点
+	for(i=0;i<pos;i++)
+	{
+		pCurrent=pCurrent->next;
+	}
+	
+	//缓存删除的结点
+	pDel=pCurrent->next;
+	pCurrent->next=pDel->next;
+	//释放删除结点的内存
+	free(pDel);
 
-
+	list->size--;
 }
 //获取链表的长度
 int Size_LinkList(LinkList* list)
 {
-
-	return 0;
+	return list->size;
 }
 //查找
 int Find_LinkList(LinkList* list,void* data)
 {
+	int i=0;
+	LinkNode* pCurrent=list->head->next;
+	if(NULL==list)
+	{
+		return -1;
+	}
 
-	return 0;
+	if(NULL==data)
+	{
+		return -1;
+	}
+
+	//遍历查找
+	i=0;
+	while(pCurrent!=NULL)
+	{
+		if(pCurrent->data==data)
+		{
+			break;
+		}
+		i++;
+		pCurrent=pCurrent->next;
+	}
+
+	return i;
 }
 //返回第一个结点
 void* Front_LinkList(LinkList* list)
 {
-
-
+	return list->head->next->data;
 }
 //打印链表结点
 void Print_LinkList(LinkList* list,PRINTLINKNODE print)
 {
-
-
+	LinkNode* pCurrent=list->head->next;
+	if(list==NULL)
+	{
+		return;
+	}
+	while(pCurrent!=NULL)
+	{
+		print(pCurrent->data);
+		pCurrent=pCurrent->next;
+	}
 }
 //释放链表内存
 void FreeSpace_LinkList(LinkList* list)
 {
+	LinkNode* pCurrent=list->head;
+	if(NULL==list)
+	{
+		return;
+	}
 
-
+	while(pCurrent!=NULL)
+	{
+		//缓存下一个节点
+		LinkNode *pNext=pCurrent->next;
+		free(pCurrent);
+		pCurrent=pNext;
+	}
+	//释放链表内存
+	list->size=0;
+	free(list);
 }
--- NewC_Structure/单向链表.c
+++ NewC_Structure/单向链表.c
@@ -4,9 +4,66 @@
 #include <string.h>
 #include "LinkList.h"
 
+//自定义数据类型
+typedef struct PERSON
+{
+	char name[64];
+	int age;
+	int score;
+}Person;
+
+//打印函数
+void MyPrint(void* data)
+{
+	Person* p=(Person*)data;
+	printf("Name:%s Age:%d Score:%d\n",p->name,p->age,p->score);
+}
+
 int main()
 {
-	printf("\n");
+
+	Person *ret;
+
+	//创建一个链表
+	LinkList *list=Init_LinkList();
+
+	//创建数据
+	Person p1={"aaa",18,100};
+	Person p2={"bbb",17,90};
+	Person p3={"ccc",16,80};
+	Person p4={"ddd",20,92};
+	Person p5={"fff",21,59};
+	
+	
+
+	//数据插入链表
+	Insert_LinkList(list,0,&p1);
+	Insert_LinkList(list,0,&p2);
+	Insert_LinkList(list,0,&p3);
+	Insert_LinkList(list,0,&p4);
+	Insert_LinkList(list,0,&p5);
+	
+	
+
+	//打印
+	Print_LinkList(list,MyPrint);
+	
+	printf("-------------\n");
+
+	//删除
+	RemoveByPos_LinkList(list,3);
+
+	//打印
+	Print_LinkList(list,MyPrint);
+	
+	printf("-------------\n");
+	
+	//返回第一个结点
+	ret=(Person*)Front_LinkList(list);
+	MyPrint(ret);
+	//销毁链表
+	FreeSpace_LinkList(list);
+
 	system("pause");
 	return 0;
 }
